ee00290ec3a9bc162b0e3f09ce4b68194961df8c
da_her <business@davidhernantes.com>
2016-09-25 13:18:50 +0200

    FizzBuzz: Iteration 5

--- Fizzbuzz/Fizzbuzz.rb
+++ Fizzbuzz/Fizzbuzz.rb
@@ -2,15 +2,16 @@
 
 # funcion fizzbuzz sin parámetros
 
-def CadenaSalida(numero)
+def CadenaSalida(numero, reglas)
 
       salida =""
 	  # comprobamos si es multiplo de 3
-	  if numero.modulo(3) == 0 
+
+	  if numero.modulo(reglas["Fizz"]) == 0 
 	       salida = "Fizz "
 	  end
 	  # comprobamos si es multiplo de 5
-	  if numero.modulo(5) == 0 
+	  if numero.modulo(reglas["Buzz"]) == 0 
 	       salida = salida + "Buzz "
 	  end
 	  # comprobamos si ha sido multiplo de 3 o de 5 o de ambos
@@ -24,16 +25,30 @@ end
 
 # llamamos a la función
 
-def FizzBuzz(inicio, fin)
+def FizzBuzz(reglas)
 
-	for num in (inicio..fin)
+	for num in (reglas["Ini"]..reglas["Fin"])
 		  
 		  # escribimos la salida
-		  puts CadenaSalida(num)
+		  puts CadenaSalida(num,reglas)
 
 	end
 
 end
 
-FizzBuzz(1, 100)
+rules = {}
+
+puts "Introduce número inicial"
+rules["Ini"] = gets.chomp.to_i
+
+puts "Introduce número final"
+rules["Fin"] = gets.chomp.to_i
+
+puts "Introduce número Fizz"
+rules["Fizz"] = gets.chomp.to_i
+
+puts "Introduce número Buzz"
+rules['Buzz'] = gets.chomp.to_i
+
+FizzBuzz(rules)
 
