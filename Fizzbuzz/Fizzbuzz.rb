6209ff21531f22d21075f4a8afb4a823aebf3a60
da_her <business@davidhernantes.com>
2016-09-25 12:13:01 +0200

    se transforma el codigo FizzBuzz en una funcion

--- Fizzbuzz/Fizzbuzz.rb
+++ Fizzbuzz/Fizzbuzz.rb
@@ -1,24 +1,32 @@
 
 
-100.times do |num|
+# funcion fizzbuzz sin parámetros
+def fizzbuzz()
   
-  # inicializamos num a 1 (no existe el alumno 0)
-  numero = num +1
-  
-  salida =""
-  # comprobamos si es multiplo de 3
-  if numero.modulo(3) == 0 
-       salida = "Fizz "
-  end
-  # comprobamos si es multiplo de 5
-  if numero.modulo(5) == 0 
-       salida = salida + "Buzz "
-  end
-  # comprobamos si ha sido multiplo de 3 o de 5 o de ambos
-  if salida == "" 
-       salida = numero
-  end
-  # escribimos la salida
-  puts salida
+	100.times do |num|
+	  
+	  # inicializamos num a 1 (no existe el alumno 0)
+	  numero = num +1
+	  
+	  salida =""
+	  # comprobamos si es multiplo de 3
+	  if numero.modulo(3) == 0 
+	       salida = "Fizz "
+	  end
+	  # comprobamos si es multiplo de 5
+	  if numero.modulo(5) == 0 
+	       salida = salida + "Buzz "
+	  end
+	  # comprobamos si ha sido multiplo de 3 o de 5 o de ambos
+	  if salida == "" 
+	       salida = numero
+	  end
+	  # escribimos la salida
+	  puts salida
+
+	end
 
 end
+
+# llamamos a la función
+fizzbuzz
