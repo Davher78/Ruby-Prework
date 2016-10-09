4986b4ef527da46b9dfe982a34cfd5d54dde698c
David Hernantes <business@davidhernantes.com>
2016-10-09 22:19:44 +0200

    prueba en el MAC

--- Exercise_1/Exercise_1.rb
+++ Exercise_1/Exercise_1.rb
@@ -1,5 +1,6 @@
 # solicitamos el nombre del fichero fuente
 puts "What is the source file?"
+puts "Cambio para comprobar la conexion con github desde el Mac"
 ficheroOrigen = gets.chomp
 # recuperamos el contenido del fichero origen
 contenido = IO.read(ficheroOrigen)
